c3814b2d49a654dacd50cb2a65456810138d9118
Sarthak Bhagia <sarthakbhagia1@gmail.com>
2026-09-17 23:44:38 +0530

    Delete Downloads/Tech-Rush-MVP-with-dispute-visible-fix directory

--- Downloads/Tech-Rush-MVP-with-dispute-visible-fix/android/app/src/main/kotlin/com/example/kaamsetu/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kaamsetu
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Downloads/Tech-Rush-MVP-with-dispute-visible-fix/lib/core/constants/app_constants.dart
+++
@@ -1,9 +0,0 @@
-import '../../models/job_category.dart';
-
-abstract class AppConstants {
-  static const String appName = 'KaamSetu';
-  static const String appTagline = 'Daily Workforce Dispatch & Operations System';
-  static const String appVersion = 'v1.0';
-  
-  static List<String> get categories => AppCategories.categoryIdsWithAll;
-}
